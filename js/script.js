158e79eeb53d0aafae4ea22bd6cc0949c7e14458
wishtree-hkshirsagar <hrishikesh.kshirsagar@wishtreetech.com>
2022-02-24 19:03:39 +0530

    added responsive behavior to home page till influencer section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(500).fadeOut('slow');
 });
+
+var windowsize = $(window).width();
+console.log(windowsize);
+// if(windowsize < 768)
